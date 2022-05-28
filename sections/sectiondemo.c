c9d081326999b8cef2a7119594bb3d8996ea8ccb
sonal patil <sonalpatil68.sp@gmail.com>
2022-05-28 22:05:47 +0530

    sectiondemo

---
+++ sections/sectiondemo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int a;
+int b = 10;
+
+int main(int argc,char const* argv[])
+{
+    int result = 0;
+    int *p = NULL;
+    p = (int*)malloc(sizeof(int));
+    add(10,20);
+    return 0; 
+}
+int add(int a,int b)
+{
+    return a + b;
+}
