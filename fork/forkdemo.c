15a12939eda619d6ece0f0918d483387b97d5237
sonal patil <sonalpatil68.sp@gmail.com>
2022-06-02 04:17:14 +0530

    Demo of fork

---
+++ fork/forkdemo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc,char const*argv[])
+{
+    pid_t id;
+
+    printf("Before fork\n"); 
+
+    id = fork();
+
+    if(0==id)
+    {
+        printf("Child Process id :%d PPID: %d\n",getpid(),getppid()); 
+    }
+    else if(0 < id)
+    {
+        printf("Parent Process id :%d PPID: %d\n",getpid(),getppid()); 
+    }
+    else 
+    {
+        perror("Fork failed\n");
+    }
+    return 0;
+}
