9527a3da56227cc228a6c86c7826b6c5cfe217f7
sonal patil <sonalpatil68.sp@gmail.com>
2022-06-02 04:22:35 +0530

    Demo of orphan

---
+++ orphan/orphandemo.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc,char const*argv[])
+{
+    pid_t id;
+
+    printf("Before fork\n"); 
+
+    id = fork();
+
+    if(0==id)
+    {
+        printf("Child Process id :%d PPID: %d\n",getpid(),getppid());
+        sleep(10); 
+        printf("Child Process id :%d PPID: %d\n",getpid(),getppid());
+    }
+    else if(0 < id)
+    {
+        printf("Parent Process id :%d PPID: %d\n",getpid(),getppid());
+       
+        printf("Parent process completed"); 
+    }
+    else 
+    {
+        perror("Fork failed\n");
+    }
+    return 0;
+}
